47b480cb28d7ac8dd53293920fac472487f7a89d
Sun Jiao <P.ozzy@outlook.com>
2025-01-04 18:22:20 +0800

    minor fixes

--- lib/entities/map_tiles.dart
+++ lib/entities/map_tiles.dart
@@ -3,7 +3,7 @@ import 'package:flutter_map/flutter_map.dart';
 import 'cache_tile_provider.dart'; // Suitable for most situations
 
 class MapTiles {
-  static const _packageName = 'com.example.birdid';
+  static const _packageName = 'net.sunjiao.birdid';
 
   static final TileLayer osm = TileLayer(
     tileProvider: CacheTileProvider('osm'),
